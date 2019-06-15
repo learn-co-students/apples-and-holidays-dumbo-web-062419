7980a7bf0ab74b64e8504762c5c61158948d6d3d
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-15 14:04:30 -0400

    finished manipulation lab

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,36 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each do |key, value|
+  	value << supply
+  end
+  holiday_hash
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
+  holiday_hash
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +58,21 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holyd|
+  	puts "#{season.to_s.capitalize}:"
+  	holyd.each do |holiday, supplies|
+  		cap_hol = holiday.to_s.split("_").collect{ |word| word.capitalize }.join(" ")
+  		suppl_list = supplies.inspect.split("\"").join
+  		suppl_list = suppl_list[1...(suppl_list.length - 1)]
+  		puts "  #{cap_hol}: #{suppl_list}"
+  	end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.values.map{|holiday| holiday.keys[0] if holiday.values[0].include?("BBQ")}.select{|elem| elem != nil}
 end
 
 
